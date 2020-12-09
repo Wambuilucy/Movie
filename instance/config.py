9795ae9999b4726f028d5dd346c37e7d3d30a7af
Wambuilucy <wambuilucie99@gmail.com>
2020-12-09 23:16:25 +0300

    first commit

---
+++ instance/config.py
@@ -0,0 +1,7 @@
+MOVIE_API_KEY = '<Your Api Key>'
+class Config:
+    '''
+    General configuration parent class
+    '''
+    MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
+
